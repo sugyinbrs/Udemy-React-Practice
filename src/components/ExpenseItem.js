7c3cebbdf6b3449af9b6316cc4e7759d2feec78e
sugyinbrs <sgnglee@gmail.com>
2022-01-20 04:06:53 +0900

    add: Components 폴더와 하위에 ExpenseItem 파일 생성

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>January 20th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
