2a0a1e1a6a4121c40d33c284be763c9f758f4104
sugyinbrs <sgnglee@gmail.com>
2022-02-03 00:47:55 +0900

    add: 조건부 렌더링을 통하여 cancel 버튼 및 form 제출 시 초기 화면으로 이동

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -79,9 +79,12 @@ const ExpenseForm = props => {
             onChange={dateChangeHandler}
           />
         </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
