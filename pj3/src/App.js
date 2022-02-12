e03657c051cfb395dd3de8fe2a2351f16eccc20d
sugyinbrs <sgnglee@gmail.com>
2022-02-12 16:35:38 +0900

    fix: 내장된 개발자 도구에서 breakpoint 로 에러 수정

--- pj3/src/App.js
+++ pj3/src/App.js
@@ -13,12 +13,14 @@ const App = () => {
   const addGoalHandler = (enteredText) => {
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: "goal1" });
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
   // 1) 이벤트들의 전체 연쇄를 살펴보며 해당 오류를 짚어낼 수 있음
   // 2) Dev Tool -> Console Tab 에 에러 메세지를 읽어낼 수 있음
+  // 3-1) Dev Tool -> Source -> Debugging Tool 에서 중단점을 활용하여 코드를 단계적으로 살펴보고 에러를 찾아서 수정할 수도 있음
+  // 3-2) 변수들에 저장된 값도 살펴보면서 (mousehover) 특정한 상황이 나타난 원인을 파악해볼 수 있음
 
   const deleteItemHandler = (goalId) => {
     setCourseGoals((prevGoals) => {
