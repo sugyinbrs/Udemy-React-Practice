0e1bed63d0d02a3215dde7acbf6676525f58474d
sugyinbrs <sgnglee@gmail.com>
2022-02-13 17:50:25 +0900

    add: 값을 입력할 수 있는 AddUser 파일 생성

--- pj4/src/components/Users/AddUser.js
+++ pj4/src/components/Users/AddUser.js
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddUser = () => {};
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const userData = {
+      username: enteredUsername,
+      age: enteredAge,
+    };
+
+    props.onSaveUserData(userData);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className={`${styles["add-users"]}`}>
+        <div className={`${styles["add-user"]}`}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" onChange={usernameChangeHandler} />
+        </div>
+        <div className={`${styles["add-user"]}`}>
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" onChange={ageChangeHandler} />
+        </div>
+      </div>
+      <div className={`${styles["add-user__actions"]}`}>
+        <button type="submit">Add User</button>
+      </div>
+    </form>
+  );
+};
 
 export default AddUser;
