fb0d96e4d4c244ac47e9348c5399d4065d551791
sugyinbrs <sgnglee@gmail.com>
2022-03-02 23:16:17 +0900

    fix: Card Component 넓이 조절

--- pj4/src/components/UI/Card.js
+++ pj4/src/components/UI/Card.js
@@ -2,7 +2,11 @@ import styles from "./Card.module.css";
 
 const Card = (props) => {
   return (
-    <div className={`${styles.card} ${props.className}`}>{props.children}</div>
+    <div className={`${styles.center}`}>
+      <div className={`${styles.card} ${props.className}`}>
+        {props.children}
+      </div>
+    </div>
   );
 };
 
