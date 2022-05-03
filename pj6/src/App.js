2666b3548f9e25e8e1cbc3147a7a78873087f8f8
sugyinbrs <sgnglee@gmail.com>
2022-05-04 01:37:38 +0900

    Add: 리액트 Context API 사용

--- pj6/src/App.js
+++ pj6/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,13 +29,14 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
+    // AuthContext.Provider -> 내장 기능, 공급자, 감싸진 모든 컴포넌트는 이제 해당 context 에 접근할 수 있음, value 내부 객체가 변경될 때마다 모든 소비 컴포넌트에 전달됨, isLoggedIn 이 변경될 때마다 리액트에 의해 업데이트 됨
   );
 }
 
--- pj6/src/components/MainHeader/Navigation.js
+++ pj6/src/components/MainHeader/Navigation.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React from "react";
+import AuthContext from "../../store/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        );
+      }}
+    </AuthContext.Consumer>
+    // AuthContext.Consumer -> 내장 기능, 소비자, 자식(함수)을 가짐, ctx 매개변수로 auth-context.js 에 있는 기본값 객체를 가져옴, 공급자 없이 소비하는 경우에만 기본값 사용
   );
 };
 
