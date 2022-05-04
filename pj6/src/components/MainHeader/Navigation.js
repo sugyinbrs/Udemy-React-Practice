ded103231289a9b5fb18ccc6aad8c2432823ef7e
sugyinbrs <sgnglee@gmail.com>
2022-05-04 10:51:43 +0900

    Modified: consumer 대신 useContext 훅을 사용하여 Context 활용

--- pj6/src/components/MainHeader/Navigation.js
+++ pj6/src/components/MainHeader/Navigation.js
@@ -1,36 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
-    // AuthContext.Consumer -> 내장 기능, 소비자, 자식(함수)을 가짐, ctx 매개변수로 auth-context.js 에 있는 기본값 객체를 가져옴, 공급자 없이 소비하는 경우에만 기본값 사용
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
